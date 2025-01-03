8f3689efa1c408c1e6e790ee1520a339dcf40515
Brandon Whitlow <nc77bwhitlow@gmail.com>
2025-01-02 22:49:02 -0500

    added a flow of the website using routing to update the page. Also inlucded social media links

---
+++ wesley-landscaping/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ServicesComponent } from '../services/services.component';
+import { ReviewsComponent } from '../reviews/reviews.component';
+import { ContactComponent } from '../contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
